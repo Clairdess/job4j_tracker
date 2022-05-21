3c8d89d13a5e3c38f81ee29caeee71e73cabbda8
Daniil Davydov <clairdess.github@gmail.com>
2022-05-22 08:33:00 +0900

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/encapsulation/College.java
@@ -0,0 +1,18 @@
+package ru.job4j.encapsulation;
+
+import ru.job4j.pojo.Student;
+
+import java.time.LocalDateTime;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setDate(LocalDateTime.now());
+        student.setFullName("Davydov Daniel");
+        student.setGroup("Job4j");
+        System.out.println(student.getDate());
+        System.out.println(student.getFullName());
+        System.out.println(student.getGroup());
+    }
+}
