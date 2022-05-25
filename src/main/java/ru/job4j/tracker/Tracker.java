541c724da8f3f2590ad32c5fac02e6885503dc81
Daniil Davydov <clairdess.github@gmail.com>
2022-05-25 18:14:16 +0900

    7. Метод удаления заявки Tracker.delete [#211749 #304095]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,15 +57,11 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
-
         int index = indexOf(id);
         if (index == -1) {
             return false;
         }
-        int start = index + 1;
-        int distPos = index;
-        int length = items.length - index - 1;
-        System.arraycopy(items, start, items, distPos, length);
+        System.arraycopy(items, index + 1, items, index, items.length - index - 1);
         items[size - 1] = null;
         size--;
         return true;
