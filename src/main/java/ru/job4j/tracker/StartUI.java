3125bce585ea7d0b685a2f4e424043346cb117a6
Daniil Davydov <clairdess.github@gmail.com>
2022-06-02 14:14:03 +0900

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #305513]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -103,6 +103,7 @@ public class StartUI {
                 "Exit Program"
         };
         System.out.println("Menu:");
+
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
         }
